140d2fe54cf0aac313b1c9ac8cfb69d4c1fb16d2
Harshini <cs23b1050@iiitdm.ac.in>
2025-02-09 16:21:14 +0530

    changes

--- src/app/committee/all-committees/page.jsx
+++ src/app/committee/all-committees/page.jsx
@@ -27,10 +27,10 @@ export default function AllCommitteesPage() {
   return (
     <main className="container mx-auto my-10 px-4">
       <h1 className="text-5xl text-blue-900 font-bold text-center mb-7">Committees and List of Experts</h1>
-      <h2 className="text-2xl font-bold text-blue-900 text-center mb-10">CICT 2025: Ninth IEEE International Conference Jointly organized by CFIIITs at IIITDM-Kancheepuram UP India</h2>
+      <h2 className="text-2xl font-bold text-blue-900 text-center mb-10">CICT-2025: Ninth IEEE,IIITDM Kancheepuram</h2>
       <CommitteeSection title="Patrons" members={Patrons.Patrons} />
       <CommitteeSection title="General Chair" members={GeneralChair.Members} />
-      <CommitteeSection title="Advisory committee" members={InternationalAdvisoryCommittee.ExpertsFromIndia} />
+      <CommitteeSection title="International Advisory Committee" members={InternationalAdvisoryCommittee.ExpertsFromIndia} />
       <CommitteeSection title="CFIIITS Steering Committee" members={CFIIITSSteeringCommittee.Members} />
       <CommitteeSection title="Technical Program Committee" members={TechnicalProgramCommittee.Members} />
       {/* Have to fix the below */}
--- src/app/committee/international-advisory-committee/page.jsx
+++ src/app/committee/international-advisory-committee/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import ScrollFadeIn from "@/app/(components)/scroll-animation";
+
 
 import { InternationalAdvisoryCommittee } from "@/app/data/international-advisory-committee";
 import SocialMediaCard from "@/app/(components)/SocialMediaCard";
@@ -9,14 +9,13 @@ import SocialMediaCard from "@/app/(components)/SocialMediaCard";
 export default function InternationalAdvisoryCommitteePage() {
   return (
     <main className="container mx-auto my-10 px-4">
-      <h1 className="text-5xl text-blue-900 font-bold text-center mb-7">Advisory committee</h1>  
-      <ScrollFadeIn>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center justify-items-around gap-8">
+      <h1 className="text-5xl text-blue-900 font-bold text-center mb-7">Advisory Committee</h1>
+   
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 p-4">
         {InternationalAdvisoryCommittee.ExpertsFromIndia.map((Indian, index) => (
           <SocialMediaCard key={index} person={Indian} />
         ))}
       </div>
-      </ScrollFadeIn>
     </main>
   );
 }
